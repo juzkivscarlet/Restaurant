1ac7c8bf09f51e35a84ac1a9284e09a7ad38a9bf
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-02 13:40:33 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -8,47 +8,71 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Create variables 
 // ===========================
 
-var waitlist = [];
 var reservations = [];
+var waitlist = [];
 
 var Customer = function(name,email,phone,id) {
 	this.name = name;
 	this.email = email;
 	this.phoneNumber = phone;
 	this.id = id;
-	if(reservations.length>5) waitlist.push(this);
-	else reservations.push(this);
+	if(reservations.length>4) this.waiting = true;
+	else this.waiting = false;
 };
+
+var cust1 = new Customer("Bob Ross", "bob.ross@god.net", "5", "GO-single-D");
+
+var cust2 = new Customer("Richard Simmons", "richard.simmons@aol.gov", "666", "Eggs");
+
+var cust3 = new Customer("Jason Phillips", "quadgodz6000@matrix.perc", "8008008000", "Quad God");
+
+reservations.push(cust1);
+reservations.push(cust2);
+waitlist.push(cust3);
 
 app.use(express.static(__dirname+"/public"));
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/home.html"));
+	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/tables.html"));
+app.get("/tables", function(req,res) {
+	res.sendFile(path.join(__dirname, "public/tables.html"));
 });
 
-app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/reserve.html"));
+app.get("/reserve", function(req,res) {
+	res.sendFile(path.join(__dirname, "public/reserve.html"));
 });
 
-app.get("/api/tables", function(req, res) {
-    return res.json(reservations);
+app.get("/api/tables", function(req,res) {
+	return res.json(reservations);
 });
 
-app.get("/api/waitlist", function(req, res) {
-    return res.json(waitlist);
+app.get("/api/waitlist", function(req,res) {
+	return res.json(waitlist);
+});
+
+app.post("/api/tables", function(req,res) {
+	var newCustomer = req.body;
+	console.log(newCustomer);
+	reservations.push(newCustomer);
+	res.json(newCustomer);
+});
+
+app.post("/api/waitlist", function(req,res) {
+	var newCustomer = req.body;
+	console.log(newCustomer);
+	waitlist.push(newCustomer);
+	res.json(newCustomer);
 });
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
